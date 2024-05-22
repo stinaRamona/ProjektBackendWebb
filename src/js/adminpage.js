cde8203da7fde1171829e70652658357fb900153
Stina Persson <stpe1901@student.miun.se>
2024-05-22 16:46:34 +0200

    Menyn visas även i admingränssnittet så man kan uppdatera och ta bort

--- src/js/adminpage.js
+++ src/js/adminpage.js
@@ -8,11 +8,40 @@ let adminHeaderEl = document.getElementById("adminHeader");
 let addBtnEl = document.getElementById("addBtn"); 
 //för error
 let errorDivAdminEl = document.getElementById("errorDivAdmin"); 
+//skriva ut admin meny
+let menuListAdmin = document.getElementById("menuListAdmin"); 
 
 //skriver ett meddelande vid inladdning av sidan
-function init() {
+async function init() {
+    //hälsar välkommen
     adminHeaderEl.innerHTML = "Välkommen tillbaka " + storedUser;
+
+    try {
+        let response = await fetch("https://projektbackend.onrender.com/api/menu"); 
+
+        let menu = await response.json(); 
+
+        printAdminMenu(menu); 
+
+    } catch(error) {
+        console.log("Något gick fel!" + error); 
+    }
 } 
+
+//hämtar meny och skriver ut så man kan uppdatera och ta bort 
+function printAdminMenu(menu){
+    
+    menu.forEach(menuItem => {
+        let item = document.createElement("article"); 
+
+        item.innerHTML = `
+        ${menuItem.dishName}<br>
+        ${menuItem.price}<br>
+        ${menuItem.description}<br>`
+
+        menuListAdmin.appendChild(item); 
+    });
+}
 
 //hämta värden från formulär
 addBtnEl.addEventListener('click', (event) => {
@@ -56,6 +85,20 @@ async function addToMenu(dishName, price, description, allergens){
     } catch(error) {
         console.error("Något gick fel" + error); 
     }
+}
+
+//ta bort något från menyn 
+async function deleteFromMenu(){
+
+    //behöver id från menuItem
+
+}
+
+//Ändra något på menyn 
+async function updateMenuItem(){
+    
+    //behöver läsa in från body och uppdatera det som är nytt. 
+    //id behövs också här 
 }
 
 
