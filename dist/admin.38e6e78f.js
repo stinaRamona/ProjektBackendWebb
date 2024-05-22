44ac90b4e5e404dac8fed33de93c877125fda762
Stina Persson <stpe1901@student.miun.se>
2024-05-22 16:18:45 +0200

    Värden kan hämtas från formulär på admin sidan

--- dist/admin.38e6e78f.js
+++ dist/admin.38e6e78f.js
@@ -589,7 +589,27 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 let storedUser = localStorage.getItem("user");
 let storedToken = localStorage.getItem("token");
 let adminHeaderEl = document.getElementById("adminHeader");
-adminHeaderEl.innerHTML = "V\xe4lkommen tillbaka " + storedUser; //Ändra meny 
+//för formulär 
+let addBtnEl = document.getElementById("addBtn");
+//för error
+let errorDivAdminEl = document.getElementById("errorDivAdmin");
+//skriver ett meddelande vid inladdning av sidan
+function init() {
+    adminHeaderEl.innerHTML = "V\xe4lkommen tillbaka " + storedUser;
+}
+//hämta värden från formulär
+addBtnEl.addEventListener("click", (event)=>{
+    event.preventDefault(); //så att inte sidan laddas om 
+    let dishNameStr = document.getElementById("dishName").value;
+    let priceStr = document.getElementById("price").value;
+    let descriptionStr = document.getElementById("description").value;
+    let allergensStr = document.getElementById("allergens").value;
+    if (dishNameStr == "" || priceStr == "" || descriptionStr == "") errorDivAdminEl.innerHTML = "Du m\xe5ste skriva i namn, pris och beskrivning";
+    else addToMenu(dishNameStr, priceStr, descriptionStr, allergensStr);
+});
+async function addToMenu(dishName, price, description, allergens) {}
+//initiering av sidan 
+window.onload = init();
 
 },{}]},["kdkSN","fNvkp"], "fNvkp", "parcelRequire8844")
 
--- src/js/adminpage.js
+++ src/js/adminpage.js
@@ -2,12 +2,40 @@
 
 //Hämta in från local storage
 let storedUser = localStorage.getItem('user'); 
-
 let storedToken = localStorage.getItem('token');
-
 let adminHeaderEl = document.getElementById("adminHeader");  
+//för formulär 
+let addBtnEl = document.getElementById("addBtn"); 
+//för error
+let errorDivAdminEl = document.getElementById("errorDivAdmin"); 
 
-adminHeaderEl.innerHTML = "Välkommen tillbaka " + storedUser; 
+//skriver ett meddelande vid inladdning av sidan
+function init() {
+    adminHeaderEl.innerHTML = "Välkommen tillbaka " + storedUser;
+} 
 
-//Ändra meny 
+//hämta värden från formulär
+addBtnEl.addEventListener('click', (event) => {
+    event.preventDefault(); //så att inte sidan laddas om 
 
+    let dishNameStr = document.getElementById("dishName").value; 
+    let priceStr = document.getElementById("price").value; 
+    let descriptionStr = document.getElementById("description").value; 
+    let allergensStr = document.getElementById("allergens").value; 
+
+    if(dishNameStr == "" || priceStr == "" || descriptionStr == ""){
+        errorDivAdminEl.innerHTML = "Du måste skriva i namn, pris och beskrivning"
+    } else {
+        addToMenu(dishNameStr, priceStr, descriptionStr, allergensStr); 
+    }
+}); 
+
+async function addToMenu(dishName, price, description, allergens){
+
+    
+}
+
+
+
+//initiering av sidan 
+window.onload = init()
