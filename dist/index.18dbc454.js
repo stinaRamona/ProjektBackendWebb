1b0de48db9b51b180bec236c03b7f42f11c39de1
Stina Persson <stpe1901@student.miun.se>
2024-05-22 11:29:05 +0200

    kan läsa ut meny från api på render

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -584,6 +584,31 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+"use strict";
+//Hämtar in element 
+let menuDivEl = document.getElementById("menuDiv");
+//Hämta data från API 
+async function getMenu() {
+    try {
+        let response = await fetch("https://projektbackend.onrender.com/api/menu");
+        let menu = await response.json();
+        printMenu(menu);
+    } catch (error) {
+        console.log("N\xe5got gick fel!" + error);
+    }
+}
+//läser ut menyn från API
+function printMenu(menu) {
+    menu.forEach((menuItem)=>{
+        let item = document.createElement("article");
+        item.innerHTML = `
+        ${menuItem.dishName}<br>
+        ${menuItem.price}<br>
+        ${menuItem.description}<br>`;
+        menuDivEl.appendChild(item);
+    });
+}
+window.onload = getMenu();
 
 },{}]},["j2YDk","1SICI"], "1SICI", "parcelRequire8844")
 
---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+"use strict"; 
+
+//Hämtar in element 
+let menuDivEl = document.getElementById("menuDiv"); 
+
+//Hämta data från API 
+async function getMenu(){
+    try {
+        let response = await fetch("https://projektbackend.onrender.com/api/menu"); 
+
+        let menu = await response.json(); 
+
+        printMenu(menu); 
+
+    } catch(error) {
+        console.log("Något gick fel!" + error); 
+    }
+}
+
+//läser ut menyn från API
+function printMenu(menu){
+    
+    menu.forEach(menuItem => {
+        let item = document.createElement("article"); 
+
+        item.innerHTML = `
+        ${menuItem.dishName}<br>
+        ${menuItem.price}<br>
+        ${menuItem.description}<br>`
+
+        menuDivEl.appendChild(item); 
+    });
+}
+
+
+window.onload = getMenu(); 
